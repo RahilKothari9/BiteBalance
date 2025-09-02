dd8f0df3d427542faa17d8601c8b9d8a2bb7b906
Rahil Kothari <110282686+RahilKothari9@users.noreply.github.com>
2025-09-02 12:15:05 +0530

    Replace invalid Eco icon import with Nature icon in SeasonalRecommendations.jsx

---
+++ path/to/SeasonalRecommendations.jsx
@@ -0,0 +1,9 @@
+import { Nature } from '@mui/icons-material';
+
+// Other imports and code...
+
+const SeasonalRecommendations = () => {
+    // Your component code here...
+};
+
+export default SeasonalRecommendations;
